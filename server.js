b35debebf89b08b5d9161ae9b1e378e377d755eb
s6352410016 <s6352410016@sau.ac.th>
2023-11-27 18:24:30 +0700

    Complete 27/11/2023

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, './client/build')));
-app.get('/*', (req, res, next) => {
+app.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'), (err) => {
         if (err) {
             res.status(500).json(err);
